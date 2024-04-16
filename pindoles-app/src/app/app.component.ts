f17165393c8f390f475cfd79d4856fe9c3f31030
jaumevc@me.com <jaumevc@me.com>
2024-04-16 21:17:32 +0200

    video 4

--- pindoles-app/src/app/app.component.ts
+++ pindoles-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // selector: 'app-root',
+  selector: 'app-init',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pindoles-app';
+  salutacio = "Hola nova propietat per afegir al component html.";
 }
