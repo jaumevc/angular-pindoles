5b691738bd9df96d8358ea6fb836fd4705913600
jaumevc@me.com <jaumevc@me.com>
2024-04-24 23:11:37 +0200

    video 5 fins minut 16:49, crear component Empleats

--- pindoles-app/src/app/app.module.ts
+++ pindoles-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleatsComponent } from './empleats/empleats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleatsComponent
   ],
   imports: [
     BrowserModule,
--- pindoles-app/src/app/empleats/empleats.component.ts
+++ pindoles-app/src/app/empleats/empleats.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-empleats",
-    
+    templateUrl: './empleats.component.html',
+    styleUrl: './empleats.component.css'
 })
 export class EmpleatsComponent {
     
