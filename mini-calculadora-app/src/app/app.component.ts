8a77ae2f82bc1af86aed56edd1d74a739be812a6
jaumevc@me.com <jaumevc@me.com>
2024-05-21 22:48:04 +0200

    video 12 pràctica 1

--- mini-calculadora-app/src/app/app.component.ts
+++ mini-calculadora-app/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mini-calculadora-app';
+  title = 'Mini Calculadora';
+  num1:number=0;
+  num2:number=0;
+  result:number=0;
+
+  sumar():void{
+    this.result = Number(this.num1) + Number(this.num2);
+  }
+
+  restar():void{
+    this.result = this.num1-this.num2;
+  }
+
+  multiplicar():void{
+    this.result = this.num1*this.num2;
+  }
+
+  dividir():void{
+    this.result = this.num1/this.num2;
+  }
 }
