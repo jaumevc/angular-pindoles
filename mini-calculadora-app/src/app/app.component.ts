ffa0d846b414346496913d8407b2385e336a7733
jaumevc@me.com <jaumevc@me.com>
2024-05-23 21:22:21 +0200

    video 13 instalar bootstrap a partir min 9

--- mini-calculadora-app/src/app/app.component.ts
+++ mini-calculadora-app/src/app/app.component.ts
@@ -9,21 +9,28 @@ export class AppComponent {
   title = 'Mini Calculadora';
   num1:number=0;
   num2:number=0;
-  result:number=0;
+  //  num1:string= "0";
+  //  num2:string= "0";
+   result:number=0;
 
   sumar():void{
-    this.result = Number(this.num1) + Number(this.num2);
+    // this.result = Number(this.num1) + Number(this.num2);
+    // this.result = parseInt(this.num1) + parseInt(this.num2);
+    this.result = this.num1 + this.num2;
   }
 
   restar():void{
     this.result = this.num1-this.num2;
+    //  this.result = parseInt(this.num1)-parseInt(this.num2);
   }
 
   multiplicar():void{
     this.result = this.num1*this.num2;
+    // this.result = parseInt(this.num1)*parseInt(this.num2);
   }
 
   dividir():void{
     this.result = this.num1/this.num2;
+    // this.result = Number(this.num1)/Number(this.num2);
   }
 }
