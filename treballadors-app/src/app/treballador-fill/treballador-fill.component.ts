15413594441f781ed5c4793033709ccd8e8783f7
jaumevc@me.com <jaumevc@me.com>
2024-06-02 13:37:22 +0200

    video 19 @Input

--- treballadors-app/src/app/treballador-fill/treballador-fill.component.ts
+++ treballadors-app/src/app/treballador-fill/treballador-fill.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Treballador } from '../treballador.model';
 
 @Component({
   selector: 'app-treballador-fill',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './treballador-fill.component.css'
 })
 export class TreballadorFillComponent {
+
+  @Input() currant:Treballador;
+  @Input() k:number;
 
 }
