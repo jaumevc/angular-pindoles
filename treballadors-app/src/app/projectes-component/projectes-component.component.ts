69cf16c9047773c3392a24a9dc80893a5cb0de36
jaumevc@me.com <jaumevc@me.com>
2024-07-09 18:07:06 +0200

    V26 routing programàtic

--- treballadors-app/src/app/projectes-component/projectes-component.component.ts
+++ treballadors-app/src/app/projectes-component/projectes-component.component.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DadesTreballadors } from '../serveis/dades.service';
+import { Treballador } from '../treballador.model';
 
 @Component({
   selector: 'app-projectes-component',
   templateUrl: './projectes-component.component.html',
   styleUrl: './projectes-component.component.css'
 })
-export class ProjectesComponentComponent {
+export class ProjectesComponentComponent implements OnInit {
 
-  constructor(private router:Router){}
+  txtfNom:string="";
+  txtfCognom:string="";
+  txtfCarrec:string="";
+  txtfsou:number=0;
+  treballadors:Treballador[]=[];
+
+  constructor(private router:Router,
+    private serveiDades:DadesTreballadors
+  ){}
+
+  ngOnInit(): void {
+  }
 
   tornarHome(){
     //posem "" perque es la ruta del home que hem posat en el href del navbar
     this.router.navigate([""]); 
+  }
+
+  addNewWorker(){
+    const nouTreballador = new Treballador(this.txtfNom, this.txtfCognom, this.txtfCarrec, this.txtfsou);
+    this.serveiDades.addTreballadorServei(nouTreballador).subscribe(data => {
+      this.treballadors.push(data);
+    }, error => {
+      console.error("Error en afegir el treballador", error);
+    });
+
+    this.tornarHome();
   }
 }
