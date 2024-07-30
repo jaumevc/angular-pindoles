02cb6e17131577249165ca3201b6793971317248
jaumevc@me.com <jaumevc@me.com>
2024-07-30 14:22:47 +0200

    vista registre

--- treballadors-app/src/app/registrar/registrar.component.ts
+++ treballadors-app/src/app/registrar/registrar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DadesTreballadors } from '../serveis/dades.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -9,8 +10,18 @@ import { DadesTreballadors } from '../serveis/dades.service';
 })
 export class RegistrarComponent {
 
+  userName:string; 
+	password:string; 
+  email:string;
+  phone:string; //llargada del phone = 12: +34973973333
+
   constructor(private serveiDades: DadesTreballadors,
     private router: Router) { }
 
   ngOnInit(): void { }
+
+  onSubmit(form:NgForm){
+    const email = form.value.email;
+    const password = form.value.password;
+  }
 }
--- treballadors-app/src/app/userLogin.model.ts
+++ treballadors-app/src/app/userLogin.model.ts
@@ -1,23 +1,11 @@
 export class UserLogin {
 
-    userName:string; 
 	password:string; 
     email:string;
-    //llargada del phone = 12
-    phone:string;
 
-    constructor(password:string,email:string,userName?:string,phone?:string){
+    constructor(password:string,email:string){
 	    this.password=password;
         this.email=email; 
-
-        if(userName!== undefined){
-            this.userName=userName;
-        }
-
-        if(phone!== undefined &&  phone.length === 12){
-            this.phone=phone;
-        }
-
     }
     
 }
